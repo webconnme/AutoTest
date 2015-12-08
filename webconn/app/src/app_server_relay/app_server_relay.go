8719c1bf2f5b76c7d37425948298720e06a57c5e
hyejin <hyejin@falinux.com>
2015-12-08 12:32:33 +0900

    .

--- webconn/app/src/app_server_relay/app_server_relay.go
+++ webconn/app/src/app_server_relay/app_server_relay.go
@@ -1,1 +1,169 @@
 package main
+
+
+import (
+	"fmt"
+	"github.com/go-martini/martini"
+	"log"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"ioctl"
+	"unsafe"
+	"os"
+)
+
+type jconfig struct {
+	Command	string `json:"command"`
+	Data	string `json:"data"`
+}
+
+type GpioS struct {
+	io int
+	mode int
+	value int
+}
+
+const (
+	IOCTL_GPIO_SET_OUTPUT = 7239681
+	IOCTL_GPIO_GET_OUTPUT = 7239682
+)
+
+var gpioPath string
+var file *os.File
+
+var chanmapchan = make(chan map[string]chan []jconfig, 1)
+
+func GPIOOpen() {
+	var err error
+	file, err = os.OpenFile(gpioPath, os.O_RDWR | os.O_SYNC, 0777)
+	if err != nil {
+		log.Fatal("open", err)
+	}
+	log.Println("GPIO Open...");
+
+}
+
+func GPIOClose() {
+	file.Close()
+	fmt.Println("GPIO Close...")
+}
+
+func runMartini(receiving chan []jconfig) {
+	m := martini.Classic()
+
+	chanmap := make(map[string]chan []jconfig)
+//	chanmap["GET"] = sending
+	chanmap["POST"] = receiving
+	chanmapchan <- chanmap
+
+	m.Group("/v01", func(r martini.Router) {
+//		r.Get("/relay/:port", martiniSender)
+		r.Post("/relay/:port", martiniReceiver)
+	})
+
+	m.RunOnAddr(":3007")
+}
+
+func martiniReceiver(r *http.Request, params martini.Params) string {
+	if r.Body == nil {
+		log.Println("http.Request.Body is empty")
+		return ""
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("ioutil.ReadAll():", err)
+		return ""
+	}
+
+	fmt.Println(">>>POST:", string(data))
+
+	var u []jconfig
+
+	err = json.Unmarshal(data, &u)
+	if err != nil {
+		log.Println("json.Unmarshal():", err)
+		return ""
+	}
+
+	if !check(u) {
+		log.Println("Invalid Command", u)
+		return ""
+	}
+
+	m := <-chanmapchan
+	c := m["POST"]
+	chanmapchan <- m
+
+	select {
+	case toDeliver := <-c:
+		fmt.Println(">>>append channel")
+		toDeliver = append(toDeliver, u...)
+		c <- toDeliver
+	default:
+		fmt.Println(">>>put channel")
+		c <- u
+	}
+
+	log.Println("OK")
+
+	return "OK"
+}
+
+func check(array []jconfig) bool {
+	for _, u := range array {
+		cmd := u.Command
+
+		switch cmd {
+		case "power":
+			return true
+		default:
+			return false
+		}
+	}
+
+	return false
+}
+
+func GpioOut(ch chan []jconfig) {
+
+	g := GpioS {0, 0, 0}
+	header := unsafe.Pointer(&g)
+	g.io = 28
+
+	for {
+
+		buf := <-ch
+
+		fmt.Println(">>>Send String: ",buf)
+		for _, tmp := range buf {
+			if tmp.Data == "on" {
+				g.value = 1
+			} else if tmp.Data == "off" {
+				g.value = 0
+			}
+			ioctl.IOCTL(uintptr(file.Fd()), IOCTL_GPIO_SET_OUTPUT, uintptr(header))
+
+		}
+
+		fmt.Println(">>>power stat : ", g.value)
+	}
+}
+
+func main() {
+
+//	s2m := make(chan []jconfig, 1)
+	m2g := make(chan []jconfig, 1)
+
+	gpioPath = "/dev/ioctl_gpio"
+	GPIOOpen()
+
+	go runMartini(m2g)
+	go GpioOut(m2g)
+
+	fmt.Scanln()
+
+	GPIOClose()
+
+}
--- webconn/app/src/app_server_rs232/app_server_rs232.go
+++ webconn/app/src/app_server_rs232/app_server_rs232.go
@@ -1,1 +1,226 @@
 package main
+
+
+import (
+	"fmt"
+	"github.com/go-martini/martini"
+	"github.com/mikepb/go-serial"
+	"log"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
+	"time"
+)
+
+type jconfig struct {
+	Command	string `json:"command"`
+	Data	string `json:"data"`
+}
+
+
+var RS232options = serial.Options{
+
+	BitRate	 	: 115200,
+	DataBits 	: 8,
+	Parity	 	: serial.PARITY_NONE,
+	StopBits 	: 1,
+	FlowControl : serial.FLOWCONTROL_NONE,
+	Mode		: serial.MODE_READ_WRITE,
+}
+
+var RS232Path string
+var serialPort *serial.Port
+
+var chanmapchan = make(chan map[string]chan []jconfig, 1)
+
+func RS232Open() {
+
+	options := RS232options
+
+	var err error
+	if serialPort != nil {
+		serialPort.Close()
+	}
+
+	serialPort, err = options.Open(RS232Path)
+	if err != nil {
+		log.Fatal("serial open ",err)
+	} else {
+		fmt.Println("serial open...")
+	}
+}
+
+func RS232Close() {
+	if serialPort != nil {
+		serialPort.Close()
+		fmt.Println("serial close...")
+	}
+}
+
+func runMartini(sending, receiving chan []jconfig) {
+	m := martini.Classic()
+
+	chanmap := make(map[string]chan []jconfig)
+	chanmap["GET"] = sending
+	chanmap["POST"] = receiving
+	chanmapchan <- chanmap
+
+	m.Group("/v01", func(r martini.Router) {
+		r.Get("/rs232/:port", martiniSender)
+		r.Post("/rs232/:port", martiniReceiver)
+	})
+
+	m.RunOnAddr(":3006")
+}
+
+func martiniSender() string {
+	m := <-chanmapchan
+	c := m["GET"]
+	chanmapchan <- m
+	select {
+	case buf := <-c:
+		fmt.Printf("martini send: %v", buf)
+		s, err := json.Marshal(buf)
+		if err != nil {
+			log.Println(err)
+			return ""
+		}
+		return string(s)
+	case <-time.After(time.Second * 60):
+		return "Timed Out"
+	}
+}
+
+func martiniReceiver(r *http.Request, params martini.Params) string {
+	if r.Body == nil {
+		log.Println("http.Request.Body is empty")
+		return ""
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("ioutil.ReadAll():", err)
+		return ""
+	}
+
+	fmt.Println(">>>POST:", string(data))
+
+	var u []jconfig
+
+	err = json.Unmarshal(data, &u)
+	if err != nil {
+		log.Println("json.Unmarshal():", err)
+		return ""
+	}
+
+	if !check(u) {
+		log.Println("Invalid Command", u)
+		return ""
+	}
+
+	m := <-chanmapchan
+	c := m["POST"]
+	chanmapchan <- m
+
+	select {
+	case toDeliver := <-c:
+		fmt.Println(">>>append channel")
+		toDeliver = append(toDeliver, u...)
+		c <- toDeliver
+	default:
+		fmt.Println(">>>put channel")
+		c <- u
+	}
+
+	log.Println("OK")
+
+	return "OK"
+}
+
+func check(array []jconfig) bool {
+	for _, u := range array {
+		cmd := u.Command
+
+		switch cmd {
+		case "rx":
+			return true
+		case "tx":
+			return true
+		case "rs232_option":
+			return true
+		default:
+			return false
+		}
+	}
+
+	return false
+}
+
+func RS232Rx(ch chan []jconfig) {
+
+	for {
+		remain, err := serialPort.InputWaiting()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if remain != 0 {
+
+			buf := make([]byte, remain)
+			_, err = serialPort.Read(buf)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			fmt.Println(">>>rs232 read : ", string(buf))
+
+			jconf := jconfig{
+				Command:"rx",
+				Data:string(buf),
+			}
+			m := []jconfig{jconf}
+
+			select {
+			case toDeliver := <-ch:
+				toDeliver = append(toDeliver,m...)
+				ch <- toDeliver
+			default:
+				ch <- m
+			}
+		}
+
+	}
+}
+
+func RS232Tx(ch chan []jconfig) {
+
+	for {
+		fmt.Println("waiting channel")
+		buf := <-ch
+
+		fmt.Println(">>>Send String: ",buf)
+		for _, tmp := range buf {
+			serialPort.Write([]byte(tmp.Data))
+		}
+	}
+}
+
+
+func main() {
+
+	s2m := make(chan []jconfig, 1)
+	m2s := make(chan []jconfig, 1)
+
+
+	RS232Path = "/dev/ttyS1"
+	RS232Open()
+
+	go runMartini(s2m,m2s)
+	go RS232Rx(s2m)	//get
+	go RS232Tx(m2s) //post
+
+	fmt.Scanln()
+
+	RS232Close()
+
+}
